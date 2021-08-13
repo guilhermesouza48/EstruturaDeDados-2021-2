b525704d50deda79c42ebb005e2eda8b6225e876
guilhermesouza48 <guilhermesouzacrs@hotmail.com>
2021-08-13 23:28:28 +0000

    13-08 bubble sort

--- 02_busca_binaria.py
+++ 02_busca_binaria.py
@@ -9,6 +9,9 @@
 from time import time
 from data.lista_nomes import nomes
 
+
+comps = 0 #  Declarando uma variável global
+
 def busca_binaria(lista, valor_busca):
     """
         Função que implementa o algoritmo de busca binária
@@ -18,29 +21,48 @@ def busca_binaria(lista, valor_busca):
     ini = 0 #Primeira posição
     fim = len(lista) - 1 #Última posição
 
+    global comps # Indica que estamos usando a variável declarada na linha 13
+
+    comps = 0
+
     while ini <= fim:
         meio = ( ini + fim ) // 2 #Operador / é divisão inteira
 
         #1ºcaso: Lista[meio] é igual a valor_busca
         if lista[meio] == valor_busca: #ENCONTROU!
+            comps += 1
             return meio #Meio é a posição onde valor_busca está na lista
 
         #2ºcaso: valor_busca é menor que lista[meio]
         elif valor_busca < lista[meio]:
+            comps += 2 
             fim = meio - 1 #Descarta a 2ºmetade da lista
 
         #3ºcaso: valor_busca é maior que lista[meio]
         else:
+            comps += 2
             ini = meio + 1 #Descarta a 1ºmetade da lista
 
     #4ºcaso: valor_busca não encontrado na lista
     return -1
 
 hora_ini = time()
-print(f"Posição de HOTAVIO: {busca_binaria(nomes, 'HOTAVIO')}")
+print(f"Posição de HOTAVIO: {busca_binaria(nomes, 'HOTAVIO')}, {comps} comparações")
 hora_fim = time()
 print(f"Tempo gasto procurando HOTAVIO: {(hora_fim - hora_ini) * 1000}ms")
 
 
-print(f"Posição de HOTAVIO: {busca_binaria(nomes, 'HOTAVIO')}")
-print(f"Posição de ZULEICA: {busca_binaria(nomes, 'ZULEICA')}")
+print(f"Posição de HOTAVIO: {busca_binaria(nomes, 'HOTAVIO')}, {comps} comparações")
+print(f"Posição de ZULEICA: {busca_binaria(nomes, 'ZULEICA')}, {comps} comparações")
+
+print(f"Nome exatamente no meio da lista: {nomes[len (nomes) // 2]}") 
+
+hora_ini = time()
+print(f"Posição de JEDERSON: {busca_binaria(nomes,'JEDERSON')}, {comps} comparações") 
+hora_fim = time()
+print(f"tempo gasto procurando JEDERSON: {(hora_fim - hora_ini) * 1000}ms ") 
+
+hora_ini = time()
+print(f"Posição de AARAO: {busca_binaria(nomes,'AARAO')}, {comps} comparações") 
+hora_fim = time()
+print(f"tempo gasto procurando AARAO: {(hora_fim - hora_ini) * 1000}ms ") 
---
+++ 03-bubble_sort.py
@@ -0,0 +1,26 @@
+# ALGORITMO DE ORDENAÇÃO BUBBLE SORT
+
+# Percorre a lista a ser ordenada em sucessivas  passadas, trocando elementos adjacentes
+# entre si quando o segundo for  menor que o primeiro. Efetua tantas  passadas quanto 
+# necessárias até que, na  última passada, nenhuma troca seja efetuada.
+
+def bubble_sort(lista):
+    """
+       Função que implementa o algoritmo de ordenação Bubble  sort
+    """
+    while True: # Loop eterno
+        trocou = False
+        # Loop na lista até o PENÚLTIMO elemento: len(lista) - 2
+        # Ex. em uma lista de 10 elementos , len(lista) == 10
+        # A última posição estará em len(lista) - 1, ou seja, 9
+        # A penúltima posição estará em len(lista) - 2 , ou seja, 8
+        for i in (range(lista) - 2):       # Inicia nova passada
+            if lista[i + 1] < lista[i]:    #  É necessário trocar 
+                lista[ i + 1 ], lista[i] = lista[i], lista[i + 1] # Faz a troca
+                trocou = True
+        
+        # Se houve troca, a lista está  ordenada e podemos interromper 
+        # o loop while 
+
+        if not trocou: #  trocou == FALSE
+            break # interrompe o while 
